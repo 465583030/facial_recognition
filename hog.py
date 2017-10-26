c388510da5ae02b8cddfa9af536de23aa79866a8
shreyas23 <shreyas.niradi@gmail.com>
2017-10-26 10:27:22 -0700

    Removed some test code and calculated max_gradient for RGB values

--- hog.py
+++ hog.py
@@ -2,6 +2,15 @@ from PIL import Image
 import math as m
 import numpy as np
 
+
+def norm(arr):
+    norm_val = 0
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            norm_val += m.pow(arr[i][j], 2)
+    return m.sqrt(norm_val)
+
+
 img = Image.open("test_images/color_gradient.jpg")  # .convert('LA)
 img_arr = np.array(img, "int64")
 
@@ -28,12 +37,21 @@ reds_grad = np.zeros((480, 640))
 greens_grad = np.zeros((480, 640))
 blues_grad = np.zeros((480, 640))
 
+# first m.pow term is the gradient in the x direction, second m.pow term is the gradient in the y direction
+# getting magnitude of both in order to compute the magnitude of the gradient
 for x in range(1, len(img_arr) - 1):
     for y in range(1, len(img_arr[x]) - 1):
-        reds_grad[x][y] = m.sqrt(m.pow(img_arr[x + 1][y][0] - img_arr[x - 1][y][0], 2) + m.pow(img_arr[x][y + 1][0] - img_arr[x][y - 1][0], 2))
-        greens_grad[x][y] = m.sqrt(m.pow(img_arr[x + 1][y][1] - img_arr[x - 1][y][1], 2) + m.pow(img_arr[x][y + 1][1] - img_arr[x][y - 1][1], 2))
-        blues_grad[x][y] = m.sqrt(m.pow(img_arr[x + 1][y][2] - img_arr[x - 1][y][2], 2) + m.pow(img_arr[x][y + 1][2] - img_arr[x][y - 1][2], 2))
+        reds_grad[x][y] = m.sqrt(m.pow(img_arr[x + 1][y][0] - img_arr[x - 1][y][0], 2)
+                                 + m.pow(img_arr[x][y + 1][0] - img_arr[x][y - 1][0], 2))
 
-Image.fromarray(reds_grad).show()
-Image.fromarray(greens_grad).show()
-Image.fromarray(blues_grad).show()
+        greens_grad[x][y] = m.sqrt(m.pow(img_arr[x + 1][y][1] - img_arr[x - 1][y][1], 2)
+                                   + m.pow(img_arr[x][y + 1][1] - img_arr[x][y - 1][1], 2))
+
+        blues_grad[x][y] = m.sqrt(m.pow(img_arr[x + 1][y][2] - img_arr[x - 1][y][2], 2)
+                                  + m.pow(img_arr[x][y + 1][2] - img_arr[x][y - 1][2], 2))
+
+# create parallel arrays to get the largest gradient using max function
+grads = [reds_grad, greens_grad, blues_grad]
+norms = [norm(reds_grad), norm(greens_grad), norm(blues_grad)]
+max_grad = grads[norms.index(max(norms))]
+
